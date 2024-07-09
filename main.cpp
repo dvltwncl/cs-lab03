58ab208bb071b3e469fa0960e44c295e81a03384
dasha <dashkalinina05@yandex.ru>
2024-07-10 02:11:10 +0300

    code:добавлено масштабирование

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include<string>
 using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
@@ -76,16 +76,20 @@ const auto TEXT_BASELINE = 20;
 const auto TEXT_WIDTH = 50;
 const auto BIN_HEIGHT = 30;
 const auto BLOCK_WIDTH = 10;
+const auto MAX_WIDTH = 760;
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-for (size_t i = 0; i < bins.size(); i++) {
-    const double bin_width = BLOCK_WIDTH * bins[i];
-    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bins[i]));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"black", "#aaffaa");
-    top += BIN_HEIGHT;
-}
-    //svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    //svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
+double top = 0;
+    for (size_t i = 0; i < bins.size(); i++) {
+        double bin_width = BLOCK_WIDTH * bins[i];
+        // Масштабирование ширины столбца, если она больше 760
+        if (bin_width > MAX_WIDTH) {
+            bin_width = MAX_WIDTH;
+        }
+
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bins[i]));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#FF69B4");
+        top += BIN_HEIGHT;
+    }
     svg_end();
 }
 
